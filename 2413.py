ce139316688a41747c6a4037908214bc37fdaac2
gabefaustino <gabefaustinos@gmail.com>
2024-10-17 18:27:49 -0300

    click

---
+++ 2413.py
@@ -0,0 +1,19 @@
+'''o número de pessoas que clicam no segundo link listado é o dobro de número de pessoas 
+que clicam no terceiro link listado
+'''
+
+#click_link_2 = click_link_3 * 2 == click_link_3 = click_link_2 / 2
+
+'''Nessa leitura, ele também descobriu que o número de pessoas que clicam no segundo link 
+é a metade do número de pessoas que clicam no primeiro link.
+
+'''
+
+# click_link_2 = click_link_1 / 2 == click_link_1 = click_link_2 * 2
+
+click_link_3 = int(input())
+click_link_2 = click_link_3 * 2
+click_link_1 = click_link_2 * 2
+
+
+print(click_link_1)
