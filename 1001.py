f1e991c7adf921bc62b9524ead5cde8a16867dc0
gabefaustino <gabefaustinos@gmail.com>
2024-10-17 10:38:52 -0300

    função soma

--- 1001.py
+++ 1001.py
@@ -1,5 +1,15 @@
-A = int(input())
+'''A = int(input())
 B = int(int(input()))
 X = A + B
 
-print(f"X = {X}")
+print(f"X = {X}")'''
+
+def calcular_soma(nota1, nota2):
+    soma = nota1 + nota2
+    return soma
+
+nota1 = int(input())
+nota2 = int(input())
+
+soma = calcular_soma(nota1, nota2)
+print(f"X = {soma}")
