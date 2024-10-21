ccc6595a111ef4933d16f8922a51d8e2b0c1fbda
gabefaustino <gabefaustinos@gmail.com>
2024-10-21 11:19:34 -0300

    bhaskara

---
+++ 1036.py
@@ -0,0 +1,26 @@
+def bhaskara (a, b, c):
+    if ((a != 0)):
+        delta = (b**2) - (4*a*c)
+        r1 = (-b + (delta**0.5)) / (2*a)
+        r2 = (-b - (delta**0.5)) / (2*a)
+        return r1, r2
+    else:
+        return "Impossivel calcular"
+
+
+entrada = input().split()
+
+a = float(entrada[0])
+b = float(entrada[1])
+c = float(entrada[2])
+
+
+resultado = bhaskara(a,b,c)
+
+
+if isinstance(resultado, str):   #verifica se a variável resultado (tupla do return) é do tipo str
+    print(resultado)
+else:
+    r1, r2 = resultado #python espera que resultado seja uma tupla ou uma sequencia de dois elementos
+    print(f"R1 = {r1:.5f}")
+    print(f"R2 = {r2:.5f}")
