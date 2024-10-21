aa90a12d280d0419fc499d2af09105fa71b58c93
gabefaustino <gabefaustinos@gmail.com>
2024-10-21 11:29:10 -0300

    intervalo

--- 1036.py
+++ 1036.py
@@ -1,11 +1,16 @@
 def bhaskara (a, b, c):
-    if ((a != 0)):
-        delta = (b**2) - (4*a*c)
-        r1 = (-b + (delta**0.5)) / (2*a)
-        r2 = (-b - (delta**0.5)) / (2*a)
-        return r1, r2
-    else:
-        return "Impossivel calcular"
+    
+    if (a == 0):
+        return("Impossivel calcular")
+    
+    delta = (b**2) - (4*a*c)
+
+    if (delta < 0):
+        return("Impossivel calcular")
+    
+    r1 = (-b + (delta**0.5)) / (2*a)
+    r2 = (-b - (delta**0.5)) / (2*a)
+    return r1, r2
 
 
 entrada = input().split()
